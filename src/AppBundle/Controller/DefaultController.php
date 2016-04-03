257ca53cd723c9f7ca7176f5ee3bf5e98fd4c1e1
Jeremy Bonnardel <Jeremy Bonnardel>
2016-04-04 01:08:03 +0200

    ajout d'un commentaire pour test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+//commentaire
 class DefaultController extends Controller
 {
     /**
