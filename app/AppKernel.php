2c9bb43d83b2e198e0220299ed58622cdeb2bb0f
Thomas Lenoir <thomas.lenoir@etudiant.univ-reims.fr>
2016-04-05 00:00:53 +0200

    creation de l'entité User qui hérite de l'entité User de FOSUser.
    creation de l'entité Etudiant et de l'entité Professeur.
    Creation de l'entité Matiere, (il lui manque l'attribut composition_secance[])
    Et enfin creation de l'entité ProfMatiere qui représente la table intermédiaire
    pour la relation Matiere Professeur.
    Cette dernière possède,en plus des clefs étrangères, un attribut responsable pour indiquer
    ci le professeur est responsable ou non.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOS\UserBundle\FOSUserBundle,
+            new UserBundle\UserBundle(),
+            new EDTBundle\EDTBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
