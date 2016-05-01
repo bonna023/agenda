755f3e212598221180f16a344bb1b68c1df751ec
bonna023 <jeremy.bonnardel@etudiant.univ-reims.fr>
2016-05-01 19:06:46 +0200

    Debut ajout formulaire evenement (je commit car je change de machine..)

---
+++ src/EDTBundle/Form/EvenementType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EDTBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('url')
+            ->add('bgColor')
+            ->add('fgColor')
+            ->add('cssClass')
+            ->add('datetime', 'datetime')
+            ->add('startDatetime', 'datetime')
+            ->add('endDatetime', 'datetime')
+            ->add('allDay')
+            ->add('groupes')
+            ->add('salle')
+            ->add('professeur')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EDTBundle\Entity\Evenement'
+        ));
+    }
+}
