1f1bcfdb192536222a2fba68844cf95e2d70ecfe
Thomas Lenoir <thomas.lenoir@etudiant.univ-reims.fr>
2016-05-08 20:09:27 +0200

    Maj du fixture pour créer l'admin, et suppression des anciennes fixtures.

--- src/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -2,14 +2,28 @@
 // src/EDTBundle/DataFixtures/ORM/LoadMatiere.php
 
 namespace UserBundle\DataFixtures\ORM;
-
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use UserBundle\Entity\User;
-class LoadUserData implements FixtureInterface, ContainerAwareInterface
-{
-    //.. $container declaration & setter
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
     public function load(ObjectManager $manager)
     {
         $userManager = $this->container->get('fos_user.user_manager');
